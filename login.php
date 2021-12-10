3c2f1177d80f259bdff84f89e1d2d38974373fb9
kovacmarija307@gmail.com <kovacmarija307@gmail.com>
2021-12-11 00:59:14 +0100

    dodata tabela i modal za dodavanje nove odece

--- login.php
+++ login.php
@@ -1,18 +1,18 @@
 <?php
 
-	require 'databasebroker.php';
-	require 'model/korisnik.php';
 
 
-	session_start();
+
+
+	
 	if(isset($_POST["login"])){  //ako je korisnik kliknuo dugme sa name-om login
         
 		$email = $_POST["form-username"];
 		$lozinka = $_POST["form-password"];
 		$k = new Korisnik(null,null,null,$email,null,$lozinka);
 		$result=Korisnik::ulogujSe($k,$conn);
-        
-	//	 $_SESSION["currentUser"] = $id;
+        $id = Korisnik::getIdByEmail($k,$conn);
+		 $_SESSION["currentUser"] = $id;
 		 
 		if(mysqli_num_rows($result) > 0){
 			
